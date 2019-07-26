f5097b142b008ccd7a84ea160355ff0ea8fd481a
jenshaw86 <bloo.j.shaw@gmail.com>
2019-07-25 19:34:08 -0700

    factor

--- factor.rb
+++ factor.rb
@@ -1,5 +1,16 @@
 lines = ARGF.read.split("\n")
 
-lines.each do |line|
-
+lines.each do |num|
+    num = num.to_i
+    factors = []
+    factor = 2
+    while num != 1 do
+        if num % factor == 0
+            num = num / factor
+            factors << factor
+        else
+            factor = factor + 1
+        end
+    end
+    puts factors.reverse.join("*")
 end
